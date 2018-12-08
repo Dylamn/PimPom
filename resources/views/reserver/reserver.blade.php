6310777417dccdded500e2e5dd1bfc247af3e1c1
delasalle-sio-eps-e <delasalle.sio.eps@gmail.com>
2018-12-08 16:57:03 +0100

    Modification de reserver.blade.php

--- resources/views/reserver/reserver.blade.php
+++ resources/views/reserver/reserver.blade.php
@@ -3,45 +3,50 @@
 @section('contenu')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
+            <!--<div class="col-md-8">-->
+                <div class="card" style="width: 100%">
                     <div class="card-header">{{ __('Effectuer une réservation') }}</div>
 
                     <div class="card-body">
+
                         {{ Form::open(['url' => 'reserver']) }}
-                        <div class="form-group row">
-                            {{ Form::label('nbAdultes', 'Nombre d\'adultes :') }}
-                            {{ Form::number('nbAdultes', 1, ['class' => 'col-lg-2 control-label']) }}
-                        </div>
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                {{ Form::label('nbAdultes', 'Nombre d\'adultes :', ['class' => 'col-md-5 col-form-label text-md-right']) }}
+                                {{ Form::number('nbAdultes', 1, ['class' => 'col-md-2 form-control']) }}
+                            </div>
+                            <div class="col-md-6 form-group">
 
-                        <div class="form-group row">
-                            {{ Form::label('nbEnfants', 'Nombre d\'enfants :') }}
-                            {{ Form::number('nbEnfants', 0) }}
-                        </div>
+                                {{ Form::label('nbEnfants', 'Nombre d\'enfants :', ['class' => 'col-md-5 col-form-label text-md-left']) }}
+                                {{ Form::number('nbEnfants', 0, ['class' => 'col-md-2 form-control', 'style' => 'margin-bottom: 5%']) }}
+                            </div>
+                            <p style="margin-bottom: 5%">Sélectionner un ou plusieurs équipement(s) :</p>
 
-                        <div class="form-group row">
-                            {{ Form::label('selectEquip', 'Skis') }}
-                            {{ Form::checkbox('selectEquip', 'Skis') }}
-                        </div>
+                            <div class="form-group row">
+                                {{ Form::label('selectEquip', 'Skis', ['class' => 'col-md-5 col-form-label text-md-right']) }}
+                                {{ Form::checkbox('selectEquip[]', 'Skis') }}
+                            </div>
 
-                        <div class="form-group row">
-                            {{ Form::label('selectEquip', 'Weedze') }}
-                            {{ Form::checkbox('selectEquip', 'Weedze') }}
-                        </div>
+                            <div class="form-group row">
+                                {{ Form::label('selectEquip', 'Weedze', ['class' => 'col-md-5 col-form-label text-md-right']) }}
+                                {{ Form::checkbox('selectEquip[]', 'Weedze') }}
+                            </div>
 
-                        <div class="form-group row">
-                            {{ Form::label('selectEquip', 'Snow') }}
-                            {{ Form::checkbox('selectEquip', 'Snow') }}
-                        </div>
+                            <div class="form-group row">
+                                {{ Form::label('selectEquip', 'Snow', ['class' => 'col-md-5 col-form-label text-md-right']) }}
+                                {{ Form::checkbox('selectEquip[]', 'Snow') }}
+                            </div>
 
-                        <div class="form-group row">
-                            {{ Form::label('selectEquip', 'Luge') }}
-                            {{ Form::checkbox('selectEquip', 'Luge') }}
+                            <div class="form-group row">
+                                {{ Form::label('selectEquip', 'Luge', ['class' => 'col-md-5 col-form-label text-md-right']) }}
+                                {{ Form::checkbox('selectEquip[]', 'Luge') }}
+                            </div>
                         </div>
-                        {{ Form::close() }}
+                            {{ Form::close() }}
+
                     </div>
                 </div>
-            </div>
+            <!--</div>-->
         </div>
     </div>
 @endsection
