0433b7546dfe647624c2eb0158d80f3cad03856e
canbolat-o <delasalle.sio.canbolat.o@gmail.com>
2018-12-14 21:54:13 +0100

    route equipement

--- resources/views/equipement/equipement.blade.php
+++ resources/views/equipement/equipement.blade.php
@@ -1,32 +1,33 @@
-@extends('equipement.template')
+@extends('reserver.template')
 
 @section('contenu')
-    <table class="table table-striped">
-        <thead>
-        <tr class="alert-success">
-            <td>Catégorie</td>
-            <td>Référence</td>
-            <td>Taille</td>
-            <td>Prix adulte</td>
-            <td>Prix enfant</td>
-            <td>Modifier</td>
-            <td>Supprimer</td>
-        </tr>
-        </thead>
-
-
-        @foreach($stock as $statement)
-            <tr>
-                <td>{{ $stock->libelle }}</td>
-                <td>{{ $stock->idInterne }}</td>
-                <td>{{ $stock->taille }}</td>
-                <td>{{ $stock->prixAdulte }}</td>
-                <td>{{ $stock->prixEnfant }}</td>
-                <td><span class="glyphicon glyphicon-pencil" style="color:orange"></span></td>
-                <td><span class="glyphicon glyphicon-remove" style="color:red"></span></td>
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+            <tr class="alert-success">
+                <td>Catégorie</td>
+                <td>Référence</td>
+                <td>Taille</td>
+                <td>Prix adulte</td>
+                <td>Prix enfant</td>
+                <td>Modifier</td>
+                <td>Supprimer</td>
             </tr>
-        @endforeach
+            </thead>
 
-    </table>
 
+            @foreach($stock as $statement)
+                <tr>
+                    <td>{{ $stock->libelle }}</td>
+                    <td>{{ $stock->idInterne }}</td>
+                    <td>{{ $stock->taille }}</td>
+                    <td>{{ $stock->prixAdulte }}</td>
+                    <td>{{ $stock->prixEnfant }}</td>
+                    <td><span class="glyphicon glyphicon-pencil" style="color:orange"></span></td>
+                    <td><span class="glyphicon glyphicon-remove" style="color:red"></span></td>
+                </tr>
+            @endforeach
+
+        </table>
+    </div>
 @endsection
