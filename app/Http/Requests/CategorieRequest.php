715cebd3e42f3949dbe48eb770de177a9b39bc55
canbolat-o <delasalle.sio.canbolat.o@gmail.com>
2018-12-19 18:06:00 +0100

    Categorie request, rules and message done

--- app/Http/Requests/CategorieRequest.php
+++ app/Http/Requests/CategorieRequest.php
@@ -24,14 +24,25 @@ class CategorieRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'label' => 'required|String',
+            'adultPrice' => ['required', 'Integer', 'min:0', 'regex:/([0-9]+)|([0-9]+\.[0-9]+)/'],
+            'childrenPrice' => ['required', 'Integer', 'min:0', 'regex:/([0-9]+)|([0-9]+\.[0-9]+)/'],
         ];
     }
 
     public function messages()
     {
         return [
-            //
+            'label.required' => 'Il faut renseigner un nom de catégorie',
+            'label.string' => 'Le nom de catégorie doit être renseigner en lettre.',
+
+            'adultPrice.required' => 'Un prix pour adulte doit être renseigné.',
+            'adultPrice.integer'  => 'Le prix renseigné doit être supérieur à 0.',
+            'adultPrice.min'      => 'Le prix pour adulte doit être supérieur ou égale à 0.',
+
+            'childrenPrice.required' => 'Un prix pour enfant doit être renseigné.',
+            'childrenPrice.integer'  => 'Le prix renseigné doit être supérieur à 0.',
+            'childrenPrice.min'      => 'Le prix pour enfant doit être supérieur ou égale à 0.',
         ];
     }
 }
