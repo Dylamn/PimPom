65bf90bdf498eb2ebfd51c7e88011352c1a75adc
canbolat-o <delasalle.sio.canbolat.o@gmail.com>
2018-12-19 19:13:07 +0100

    Fonction AJAX a regler pour afficher l'input du nombre d'equipement a choisir quand on clique sur le checkbox

--- resources/views/reserver/index.blade.php
+++ resources/views/reserver/index.blade.php
@@ -72,6 +72,15 @@
                                 {{ Form::number('nbChaussures', 0, ['class' => 'col-md-1 form-control', 'style' => 'display: inline', 'min' => '0']) }}
                             </div>
                         </div>
+                        <div class="row">
+                            <div class="col-md form-group">
+                                {{ Form::label('dateDebut', 'Date début réservation :', ['class' => 'col-md-6 col-form-label text-md-right']) }}
+                                {{ Form::date('dateDebut', 1, ['class' => 'col-md form-control', 'style' => 'display: inline']) }}
+                            </div>
+                            <div class="col-md form-group">
+                                {{ Form::label('dateFin', 'Date fin réservation :', ['class' => 'col-md-6 col-form-label text-md-right']) }}
+                                {{ Form::date('dateFin', 0, ['class' => 'col-md form-control', 'style' => 'margin-bottom: 5%; display: inline']) }}
+                            </div>
                         <div class="row offset-md-10">
                             <button type="submit" class="btn btn-primary">Suivant ></button>
                         </div>
@@ -81,4 +90,40 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+            <script src="">
+                siOui = $(function(){
+                    $('div.CheckBoxes input[type=checkbox]').click(function(){
+                        cochee = $(this).is(':checked'); // je regarde si la case est cochée ou non
+                        page=$(this).attr("name"); // je capture l'attibut name
+                        cible=page+".php"; // je créer une variable lien pour ma requete ajax
+
+                        if (cochee == true) {
+                            $(function(){
+                                $.ajax({
+                                    url: "contenu/"+cible, // j'utilise le lien pour aller cherche les contenus à rajouter
+                                    cache: false,
+                                    success: function(html) {
+                                        $("#Form"+page).append(html); // j'ajoute le contenu dans ma div
+                                    },
+                                    error: function(XMLHttpRequest,textStatus, errorThrown){
+                                        alert(textStatus);
+                                    },
+                                });
+                                return false;
+                            });
+                        }
+
+                        else {
+                            $(function(){
+                                $("#Form"+page).empty(); // je supprime le contenu dans ma div
+                                return false;
+                            });
+                        }
+
+                    });
+                });
+            </script>
 @endsection
