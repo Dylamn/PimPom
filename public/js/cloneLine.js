07a70509f493a5b488036c3ec200316f74ebe39f
canbolat-o <delasalle.sio.canbolat.o@gmail.com>
2019-03-24 21:17:57 +0100

    Fonvtion qui TailleSB a test.

--- public/js/cloneLine.js
+++ public/js/cloneLine.js
@@ -1,16 +1,26 @@
 (function() {
-    let nbEquip = document.getElementById("nbSnowboard");
+    let nbEquip = document.getElementById("nbrSnowboard");
 
-    nbEquip.addEventListener('change', function(v){
+    nbEquip.addEventListener('change',equip);
+
+
+    function  equip() {
         for (let i = 1; i <= nbEquip.value; i++) {
-            let clone = document.getElementById('tailleSnowboard').cloneNode(true);
-            clone.id = 'tailleSnowboard'+i;
-            document.getElementById("tailleSnowboard").append(clone);
-            console.log(clone)
+            document.getElementById('divTailleSnowboard').innerHTML =
+                "<input class=\'col-md-2 form-control\' style=\'display: inline\' min=\'0\' name=\'tailleSnowboard\'"+ i +"type=\"number\' value=\'0\' id=\'tailleSnowboard\'"+ i +">";
         }
+    }
+
+
+
+
+
+
+
+
+
     });
 
     for (let i = 0; i < nbEquip; i++) {
-        console.log('nb : '+nbEquip.value);
+        console.log('nb : ' + nbEquip.value);
     }
-})();
