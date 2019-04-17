dee4280bb8c6df556fc51cd80a29abfc38a2f1c6
canbolat-o <delasalle.sio.canbolat.o@gmail.com>
2019-04-17 16:25:47 +0200

    https://www.journaldunet.fr/web-tech/developpement/1202649-comment-passer-une-variable-de-php-a-javascript/

--- public/js/cloneLine.js
+++ public/js/cloneLine.js
@@ -7,69 +7,64 @@
 
     nbPersonnes.addEventListener('change', function (ev) {
         console.log('noway');
-       console.log(nbPersonnes.value);
+        console.log(nbPersonnes.value);
+
+        div.innerHTML ="";
 
         for (let i = 1; i <= nbPersonnes.value; i++) {
-            console.log("test"+i);
+            console.log("test" + i);
+
+            let prenomClient = document.createElement("label");
+            prenomClient.className = "col-md-2 col-form-label text-md-right";
+            prenomClient.id = "prenomClient";
+            prenomClient.setAttribute("for", "prenomClient");
+            prenomClient.innerHTML = "Prénom du client : ";
+            div.appendChild(prenomClient);
+
+            let prenomClientInput = document.createElement("input");
+            prenomClientInput.id = "prenomClient" + i;
+            prenomClientInput.className = "col-md-3 form-control";
+            prenomClientInput.style = "display: inline";
+            prenomClientInput.setAttribute("name", "prenomClient" + i.toString());
+            prenomClientInput.setAttribute("type", "text");
+            div.appendChild(prenomClientInput);
+
+
+            let inputLab = document.createElement("label");
+            inputLab.className = "col-md-1 col-form-label text-md-right";
+            inputLab.setAttribute("for", "tailleSnowboard" + i.toString());
+            inputLab.innerHTML = "Taille : ";
+            div.appendChild(inputLab);
+
+
+            let input = document.createElement("input");
+            input.id = "tailleSnowboard" + i;
+            input.className = "col-md-2 form-control";
+            input.style = "display: inline";
+            input.setAttribute("min", "0");
+            input.setAttribute("name", "tailleSnowboard" + i.toString());
+            input.setAttribute("type", "number");
+            div.appendChild(input);
+
+            let inputLabC = document.createElement("label");
+            inputLabC.className = "col-md-1 col-form-label text-md-right form-check-label";
+            inputLabC.setAttribute("for", "enfantSnowboard" + i.toString());
+            inputLabC.innerHTML = "Enfant : ";
+            div.appendChild(inputLabC);
+
+            let checkChild = document.createElement("input");
+            checkChild.id = "enfantSnowboard" + i;
+            checkChild.className = "col-md-2 form-check-input";
+            checkChild.style = "display: inline";
+            checkChild.setAttribute("name", "enfantSnowboard" + i.toString());
+            checkChild.setAttribute("value", "enfantSnowboard" + i.toString() + "[]");
+            checkChild.setAttribute("type", "checkbox");
+           div.appendChild(checkChild);
         }
+
+
     });
-    // nbPersonnes.addEventListener('change', function () {
-    //
-    //
-    //     for (let i = 1; i <= nbPersonnes.value; i++) {
-    //
-    //         let div2 = document.createElement("div");
-    //         div2.id = "divClient" + 1;
-    //         div2.className = "row";
-    //
-    //         let prenomClient = document.createElement("label");
-    //         prenomClient.className = "col-md-2 col-form-label text-md-right";
-    //         prenomClient.id = "prenomClient";
-    //         prenomClient.setAttribute("for", "prenomClient");
-    //         prenomClient.innerHTML = "Prénom du client : ";
-    //         div2.appendChild(prenomClient);
-    //
-    //         let prenomClientInput = document.createElement("input");
-    //         prenomClientInput.id = "prenomClient" + i;
-    //         prenomClientInput.className = "col-md-3 form-control";
-    //         prenomClientInput.style = "display: inline";
-    //         prenomClientInput.setAttribute("name","prenomClient" + i.toString());
-    //         prenomClientInput.setAttribute("type","text");
-    //         div2.appendChild(prenomClientInput);
-    //
-    //         div.appendChild(div2);
-    //         // let inputLab = document.createElement("label");
-    //         // inputLab.className = "col-md-1 col-form-label text-md-right";
-    //         // inputLab.setAttribute("for", "tailleSnowboard" + i.toString());
-    //         // inputLab.innerHTML = "taille : ";
-    //         // div.appendChild(inputLab);
-    //
-    //
-    //         // let input = document.createElement("input");
-    //         // input.id = "tailleSnowboard" + i;
-    //         // input.className = "col-md-2 form-control";
-    //         // input.style = "display: inline";
-    //         // input.setAttribute("min", "0");
-    //         // input.setAttribute("name", "tailleSnowboard" + i.toString());
-    //         // input.setAttribute("type", "number");
-    //         // document.getElementById('tailleSnowboard').appendChild(input);
-    //         //
-    //         // let inputLabC = document.createElement("label");
-    //         // inputLabC.className = "col-md-1 col-form-label text-md-right form-check-label";
-    //         // inputLabC.setAttribute("for", "enfantSnowboard" + i.toString());
-    //         // inputLabC.innerHTML = "Enfant : ";
-    //         // document.getElementById('tailleSnowboard').appendChild(inputLabC);
-    //         //
-    //         // let checkChild = document.createElement("input");
-    //         // checkChild.id = "enfantSnowboard" + i;
-    //         // checkChild.className = "col-md-2 form-check-input";
-    //         // checkChild.style = "display: inline";
-    //         // checkChild.setAttribute("name", "enfantSnowboard" + i.toString());
-    //         // checkChild.setAttribute("value", "enfantSnowboard" + i.toString() + "[]");
-    //         // checkChild.setAttribute("type", "checkbox");
-    //         // document.getElementById('tailleSnowboard').appendChild(checkChild);
-    //     }
-    // });
+
 
 })
 ();
